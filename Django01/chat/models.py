08c09457a909dcf8bffd0c94d5a92fdd195d746c
lpg <1739357748@qq.com>
2020-11-21 16:34:52 +0800

    公司电脑

--- Django01/chat/models.py
+++ Django01/chat/models.py
@@ -7,23 +7,23 @@ from django.db.models.base import Model
 class user(models.Model):
   name=models.CharField(max_length=20)
   gender_choise=[('F','男'),('M','女')]
-  gender=models.CharField(max_length='2',choices=gender_choise,default='F')
+  gender=models.CharField(max_length=2,choices=gender_choise,default='F')
   tel_number=models.CharField(max_length=11)
   address=models.CharField(max_length=50)
   icon=models.BinaryField()
 
 class clients(models.Model):
   channel_name=models.CharField(max_length=40)
-  user_name=models.OneToOneField(on_delete=models.CASCADE)
-  login_time=models.DateTimeField('date login')
+  user_name=models.OneToOneField(to= user, on_delete=models.CASCADE)
+  login_time=models.DateTimeField('date_login')
 
 class group(models.Model):
   name=models.CharField(max_length=20)
-  
+  icon=models.BinaryField()
 
 class user_group(models.Model):
-  user_id=models.OneToOneField(user.id, on_delete=models.CASCADE)
+  user_id=models.OneToOneField(user, on_delete=models.CASCADE)
   group_id=models.OneToOneField(group, on_delete=models.CASCADE)
-  is_owner=models.models.BooleanField()
-  is_manager=models.models.BooleanField()
+  is_owner=models.BooleanField(default=False)
+  is_manager=models.BooleanField(default=False)
 
